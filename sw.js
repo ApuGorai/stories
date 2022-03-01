ecf9481dc82ef21bb7d70889eeb5408de33c3b2e
ApuGorai <97297476+ApuGorai@users.noreply.github.com>
2022-03-01 14:45:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
 
-importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
-AMP_SW.init();
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('webstories').then((cache) => cache.addAll([
+      '/',
+    './index.html',
+    'manifest.webmanifest',
+     'sw.js',
+      'index.js',
+      '404.html',
+      './images/icon_192.png',
+     './images/icon_512.png'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
