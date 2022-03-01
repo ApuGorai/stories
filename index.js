a2a5a212b2c164380efca2ebd5e1746862d1f120
ApuGorai <97297476+ApuGorai@users.noreply.github.com>
2022-03-01 14:43:39 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(() => { console.log('Service Worker Registered'); });
+}
+
+let deferredPrompt;
+var addBtn = document.querySelector(".addbtn");
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  addBtn.addEventListener('click', school);
+  
+  function school() {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('Users download
+');
+      } else {
+        console.log('User will download');
+      }
+      deferredPrompt = null;
+    });
+    }
+});
